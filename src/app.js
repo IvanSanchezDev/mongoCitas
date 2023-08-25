2765550778e06f559e151b01885d101465ada4c6
IvanSanchezDev <ivancampus2023@gmail.com>
2023-08-25 09:06:22 -0500

    refactor: :recycle: modificacion de rutas para la nueva caracteristicas de permisos segun el rol y las versiones

--- src/app.js
+++ src/app.js
@@ -2,13 +2,15 @@ import express from 'express'
 import { appCitas } from './routes/citas.routes.js'
 import { appUsuario } from './routes/usuario.routes.js'
 import { appMedico } from './routes/medico.routes.js'
+import { appLogin } from './routes/login.routes.js'
 const app = express()
 
 app.use(express.json())
 
-app.use('/api/cita', appCitas)
-app.use('/api/medico', appMedico)
-app.use('/api/usuario', appUsuario)
+app.use('/api/paciente', appCitas)
+app.use('/api/admin', appMedico)
+app.use('/api/admin', appUsuario)
+app.use('/login', appLogin)
 
 app.use((req, res) => {
   res.status(404).json({ message: '404 route not found' })
