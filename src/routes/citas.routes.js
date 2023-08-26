2b962958830f8e94bf5b2454dcddcd6db627d2b7
IvanDeveloperCampus <ivancampus2023@gmail.com>
2023-08-25 21:16:04 -0500

    feat: :sparkles: implementacion del middleware  de autenticacion e implementacion der versionApi

--- src/routes/citas.routes.js
+++ src/routes/citas.routes.js
@@ -1,9 +1,16 @@
-import { Router } from 'express'
+import passportHelper from '../helpers/passportHelper.js'
+import Routes from 'express'
 import { Citas } from '../controllers/Citas.js'
+import routesVersioning from 'express-routes-versioning'
 
-export const appCitas = Router()
+export const appCitas = Routes()
 
-appCitas.get('/getCitas', Citas.getCitas)// APROBADA
+const version = routesVersioning()
+
+// Bearer
+appCitas.use(passportHelper.authenticate('bearer', { session: false }))
+
+appCitas.get('/getCitas', version({ '3.5.0': Citas.getCitas, '1.0.0': Citas.citaGenero }))// APROBADA
 appCitas.get('/proximaCita', Citas.proximaCita)// APROBADA
 appCitas.get('/citasFechaEspecifica', Citas.citaFechaEspecifica)// APROBADA
 appCitas.get('/citasFechaMedicoEspecifico/:nroMatricula', Citas.citasFechaMedicoEspecifico)
